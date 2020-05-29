e1b2b1f54414823f05ccba0983cdc958f386b405
Jonathan-rangel <Elperros16@>
2020-05-29 15:57:12 -0500

    Se agregó el reporte final

--- FireShipA.java
+++ FireShipA.java
@@ -1,17 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class FireShipA here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class FireShipA extends Fire
 {
-    /**
-     * Act - do whatever the FireShipA wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act() 
     {
         move(5);
